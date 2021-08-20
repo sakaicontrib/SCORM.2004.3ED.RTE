0d1a39221500973c7df62efb562574ba42781fbf
Brian Jones <bjones86@uwo.ca>
2021-08-20 09:26:39 -0400

    SCO-176 fix build for 20.x

--- scorm-impl/service/src/java/org/sakaiproject/scorm/service/sakai/impl/MockServerConfigurationService.java
+++ scorm-impl/service/src/java/org/sakaiproject/scorm/service/sakai/impl/MockServerConfigurationService.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Set;
 import java.util.regex.Pattern;
 import org.sakaiproject.component.api.ServerConfigurationService;
 
@@ -34,12 +33,6 @@ public class MockServerConfigurationService implements ServerConfigurationServic
 
     @Override
     public List<String> getCategoryGroups( String category )
-    {
-        throw new UnsupportedOperationException( "Not supported yet." );
-    }
-
-    @Override
-    public Set<String> getCommaSeparatedListAsSet( String string )
     {
         throw new UnsupportedOperationException( "Not supported yet." );
     }
